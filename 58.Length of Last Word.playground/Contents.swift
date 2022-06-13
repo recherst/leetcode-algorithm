1f418aac0f7943d51b4a8fd7ed606142d7d3e4ba
Yonggang Liu <ygliusz@gmail.com>
2022-06-13 10:44:37 +0800

    Adds a new solution of "Length of Last Word".

---
+++ 58.Length of Last Word.playground/Contents.swift
@@ -0,0 +1,38 @@
+/**
+ Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+ A word is a maximal substring consisting of non-space characters only.
+
+  
+ Example 1:
+ Input: s = "Hello World"
+ Output: 5
+ Explanation: The last word is "World" with length 5.
+ 
+ Example 2:
+ Input: s = "   fly me   to   the moon  "
+ Output: 4
+ Explanation: The last word is "moon" with length 4.
+ 
+ Example 3:
+ Input: s = "luffy is still joyboy"
+ Output: 6
+ Explanation: The last word is "joyboy" with length 6.
+  
+
+ Constraints:
+ - 1 <= s.length <= 104
+ - s consists of only English letters and spaces ' '.
+ - There will be at least one word in s.
+ */
+class Solution {
+    func lengthOfLastWord(_ s: String) -> Int {
+        guard !s.isEmpty else { return 0 }
+        let subStrArr = s.split(separator: " ")
+        return subStrArr.last!.count
+    }
+}
+
+let s = Solution()
+s.lengthOfLastWord("   fly me   to   the moon  ")
+print(s)
