064d37b5b06f389563c84d48700626b5b34a8880
Yonggang Liu <ygliusz@gmail.com>
2022-06-21 01:31:26 +0800

    New solution for question No.125.

---
+++ Easy/125.Valid Palindrome.playground/Contents.swift
@@ -0,0 +1,49 @@
+/**
+ A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+ Given a string s, return true if it is a palindrome, or false otherwise.
+
+  
+ Example 1:
+ Input: s = "A man, a plan, a canal: Panama"
+ Output: true
+ Explanation: "amanaplanacanalpanama" is a palindrome.
+ 
+ Example 2:
+ Input: s = "race a car"
+ Output: false
+ Explanation: "raceacar" is not a palindrome.
+ 
+ Example 3:
+ Input: s = " "
+ Output: true
+ Explanation: s is an empty string "" after removing non-alphanumeric characters.
+ Since an empty string reads the same forward and backward, it is a palindrome.
+  
+
+ Constraints:
+ - 1 <= s.length <= 2 * 105
+ - s consists only of printable ASCII characters.
+ */
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+        var newStr = ""
+        for c in s {
+            if ("a"..."z").contains(c.lowercased()) {
+                newStr.append(c.lowercased())
+            } else if ("0"..."9").contains(c) {
+                newStr.append(c)
+            }
+        }
+        print(newStr)
+        guard !newStr.isEmpty else { return true }
+        let preStr = String(newStr.prefix(newStr.count / 2))
+        let sufStr = String(newStr.suffix(newStr.count / 2))
+        return String(preStr.reversed()) == sufStr
+    }
+}
+
+let s = Solution()
+let r = s.isPalindrome("0P")
+print(r)
+
