7022a03c1ed08d5aebdc5160712749fbfe3b82d0
recherst <recherst@icloud.com>
2022-07-26 00:36:16 +0800

    Reformat the explanation.

--- Medium/8.String to Integer (atoi).playground/Contents.swift
+++ Medium/8.String to Integer (atoi).playground/Contents.swift
@@ -3,16 +3,16 @@
 
  The algorithm for myAtoi(string s) is as follows:
 
- Read in and ignore any leading whitespace.
- Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final strult is negative or positive strpectively. Assume the strult is positive if neither is pstrent.
- Read in next the characters until the next non-digit character or the end of the input is reached. The strt of the string is ignored.
- Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
- If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
- Return the integer as the final strult.
+    1. Read in and ignore any leading whitespace.
+    2. Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final strult is negative or positive strpectively. Assume the strult is positive if neither is pstrent.
+    3. Read in next the characters until the next non-digit character or the end of the input is reached. The strt of the string is ignored.
+    4. Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+    5. If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+    6. Return the integer as the final strult.
+ 
  Note:
-
- Only the space character ' ' is considered a whitespace character.
- Do not ignore any characters other than the leading whitespace or the strt of the string after the digits.
+ - Only the space character ' ' is considered a whitespace character.
+ - Do not ignore any characters other than the leading whitespace or the strt of the string after the digits.
   
 
  Example 1:
