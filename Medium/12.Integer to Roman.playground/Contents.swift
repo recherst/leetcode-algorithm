0e685f9ccac283d0e91a78a3afc6247409cbfdf0
recherst <recherst@icloud.com>
2022-07-26 20:49:37 +0800

    Optimized the algorithm.

--- Medium/12.Integer to Roman.playground/Contents.swift
+++ Medium/12.Integer to Roman.playground/Contents.swift
@@ -47,50 +47,39 @@ class Solution {
             if n >= 1000 {
                 n -= 1000
                 res.append("M")
-                continue
-            } else if n >= 900 {
+            } else if n >= 900 && n < 1000 {
                 n -= 900
                 res.append("CM")
-                continue
-            } else if n >= 500 {
+            } else if n >= 500 && n < 900 {
                 n -= 500
                 res.append("D")
-                continue
-            } else if n >= 400 {
+            } else if n >= 400 && n < 500 {
                 n -= 400
                 res.append("CD")
             } else if n >= 100 && n < 400 {
                 n -= 100
                 res.append("C")
-                continue
-            } else if n >= 90 {
+            } else if n >= 90 && n < 100 {
                 n -= 90
                 res.append("XC")
-                continue
-            } else if n >= 50 {
+            } else if n >= 50 && n < 90 {
                 n -= 50
                 res.append("L")
-                continue
-            } else if n >= 40 {
+            } else if n >= 40 && n < 50 {
                 n -= 40
                 res.append("XL")
-                continue
             } else if n >= 10 && n < 40 {
                 n -= 10
                 res.append("X")
-                continue
-            } else if n >= 9 {
+            } else if n >= 9 && n < 10 {
                 n -= 9
                 res.append("IX")
-                continue
-            } else if n >= 5 {
+            } else if n >= 5 && n < 9 {
                 n -= 5
                 res.append("V")
-                continue
-            } else if n >= 4 {
+            } else if n >= 4 && n < 5 {
                 n -= 4
                 res.append("IV")
-                continue
             } else if n >= 1 {
                 n -= 1
                 res.append("I")
